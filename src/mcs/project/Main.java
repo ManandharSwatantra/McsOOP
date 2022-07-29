3ac0ce23cbbb2ff4a34edf2ba9fc2b86d5779758
MdrSwatantra <swatantra2078@gmail.com>
2022-07-29 22:51:32 +0545

    Interface Used

--- src/mcs/project/Main.java
+++ src/mcs/project/Main.java
@@ -6,14 +6,14 @@ public class Main {
     public static void main(String[] args) {
         double data;
         System.out.println("Please choose any one option to pay: ");
-        System.out.println("-------------------------------");
+        System.out.println("-------------------------------------------------");
         System.out.println("1 - Cash Payment");
         System.out.println("2 - Card Payment");
         Scanner sc = new Scanner(System.in);
         int option = sc.nextInt();
         switch (option) {
             case 1:
-                System.out.println("-------------------------------");
+                System.out.println("-------------------------------------------------");
                 System.out.println("You have selected the Cash Payment");
                 System.out.println("Enter the amount to be paid: ");
                 data = sc.nextDouble();
@@ -22,7 +22,7 @@ public class Main {
                 obj.cashPaymentDetails(data);
                 break;
             case 2:
-                System.out.println("-------------------------------");
+                System.out.println("-------------------------------------------------");
                 System.out.println("You have selected the Credit Card Payment");
                 System.out.println("Enter the amount to be paid: ");
                 data = sc.nextDouble();
--- src/mcs/project/Payment.java
+++ src/mcs/project/Payment.java
@@ -2,9 +2,10 @@ package mcs.project;
 
 import java.util.Scanner;
 
-public class Payment implements CashPay, CreditCardPay{
+public class Payment implements CashPay, CreditCardPay {
     double amount;
     double charge;
+
     public double getAmount() {
         return amount;
     }
@@ -15,10 +16,13 @@ public class Payment implements CashPay, CreditCardPay{
 
     public double deliverCharge(double amount) {
         if (amount < 50) {
+            System.out.println("Delivery Charge: $10.");
             charge = this.amount + 10;
-        } else if (amount>=50 && amount<100) {
+        } else if (amount >= 50 && amount < 100) {
+            System.out.println("Delivery Charge: $5.");
             charge = this.amount + 5;
         } else {
+            System.out.println("Delivery Cost is Free.");
             charge = this.amount;
         }
         return charge;
@@ -26,34 +30,56 @@ public class Payment implements CashPay, CreditCardPay{
 
     public void cashPaymentDetails(double val) {
         setAmount(val);
-        System.out.println("-------------------------------");
-        System.out.println("The total paid amount after paying from cash is: " + "$" + deliverCharge(getAmount()));
+        System.out.println("-------------------------------------------------");
+        System.out.println("The total payable amount is: " + "$" + deliverCharge(getAmount()));
     }
 
     public void CreditCardPaymentDetails(double val) {
         setAmount(val);
-        System.out.println("-------------------------------");
+        System.out.println("-------------------------------------------------");
         System.out.println("From which card will you pay: ");
-        System.out.println("-------------------------------");
-        System.out.println("1 - Master Card or Visit Card");
-        System.out.println("2 - Amex Card");
-        System.out.println("3 - Gift Card");
+        System.out.println("-------------------------------------------------");
+        System.out.println("1 - Master Card");
+        System.out.println("2 - Visit Card");
+        System.out.println("3 - Amex Card");
+        System.out.println("4 - Gift Card");
         Scanner sc = new Scanner(System.in);
         int option = sc.nextInt();
         switch (option) {
             case 1:
-                this.amount = this.amount + (this.amount*1.5)/100;
-                System.out.println("-------------------------------");
-                System.out.println("The total paid amount after paying from Credit Card is: "+ deliverCharge(getAmount()));
+                this.amount = val + Math.round((val * 1.5) / 100);
+                System.out.println("-------------------------------------------------");
+                System.out.println("Master Card Selected.");
+                System.out.println("-------------------------------------------------");
+                System.out.println("Total Cost: " + val);
+                System.out.println("Card Charge: " + "$" + (val * 1.5) / 100);
+                System.out.println("The total payable amount is: " + "$" + deliverCharge(getAmount()));
                 break;
             case 2:
-                this.amount = this.amount + (this.amount*3)/100;
-                System.out.println("-------------------------------");
-                System.out.println("The total paid amount after paying from Credit Card is: "+ deliverCharge(getAmount()));
+                this.amount = this.amount + (this.amount * 1.5) / 100;
+                System.out.println("-------------------------------------------------");
+                System.out.println("Visit Card Selected.");
+                System.out.println("-------------------------------------------------");
+                System.out.println("Total Cost: " + "$" + val);
+                System.out.println("Card Charge: " + "$" + (val * 1.5) / 100);
+                System.out.println("The total payable amount is: " + "$" + deliverCharge(getAmount()));
                 break;
             case 3:
-                System.out.println("-------------------------------");
-                System.out.println("The total paid amount after paying from Credit Card is: "+ deliverCharge(getAmount()));
+                this.amount = this.amount + (this.amount * 3) / 100;
+                System.out.println("-------------------------------------------------");
+                System.out.println("Amex Card Selected.");
+                System.out.println("-------------------------------------------------");
+                System.out.println("Total Cost: " + "$" + val);
+                System.out.println("Card Charge: " + "$" + (val * 3) / 100);
+                System.out.println("The total payable amount is: " + "$" + deliverCharge(getAmount()));
+                break;
+            case 4:
+                System.out.println("-------------------------------------------------");
+                System.out.println("Gift Card Selected.");
+                System.out.println("-------------------------------------------------");
+                System.out.println("Total Cost: " + "$" + val);
+                System.out.println("Card Charge: FREE");
+                System.out.println("The total payable amount is: " + "$" + deliverCharge(getAmount()));
                 break;
         }
     }
